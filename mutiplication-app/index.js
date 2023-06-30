6eab461a5ded0a9c8f97baae6d7af0ca07b013e0
deepamaria <deepamaria89@gmail.com>
2023-06-30 11:54:08 +0200

    Commit1

---
+++ mutiplication-app/index.js
@@ -0,0 +1,5 @@
+const num1 = Math.ceil(Math.random()* 10);
+const num2 = Math.ceil(Math.random()* 10);
+
+const questionEl = document.getElementById("question")
+questionEl.innerText = 'What is ${num1} multiply by ${num2}?'
