54bea1588dddffeab59f5cc0707ae45d675381c0
deepamaria <deepamaria89@gmail.com>
2023-07-20 15:29:17 +0200

    Update

---
+++ order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone', 
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender Drone', 
+        productNumber: '35631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruco F11 Pro Drone', 
+        productNumber: '63122',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera', 
+        productNumber: '13234',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone', 
+        productNumber: '22435',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'DJI Air 2S Drone', 
+        productNumber: '82131',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Laosen Drone', 
+        productNumber: '92342',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
